bed06add5991d56a5f930d076921bedaa1f3e334
Allen Mathews <allenjoemathews@gmail.com>
2023-02-20 17:37:24 +1100

    commit tree 7ebe1176b9369b8df8e4ffda2f4fcba1ad74b7a1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// function superbowlWin(array) {
+//     let winYear = array.find((ob) => ob.result === 'W');
+//     if (winYear) {
+//         return winYear.year;
+//     }
+// }
+
 function superbowlWin(array) {
     let winYear = array.find((ob) => ob.result === 'W');
     if (winYear) {
